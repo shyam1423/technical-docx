6a31a04f0399d60fff22d7ea7301298672dd103b
shyam1423 <79440899+shyam1423@users.noreply.github.com>
2021-02-23 16:46:10 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'Git & Github'
+copyright = '2021, shyam'
+author = 'shyam kumar reddy'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
